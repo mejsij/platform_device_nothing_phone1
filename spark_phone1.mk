8121486d75f7de629ffc6e60c084188495ad5230
mejsij <maciek00792@o2.pl>
2022-12-19 17:01:33 +0100

    phone1: Spark bringup

--- spark_phone1.mk
+++ spark_phone1.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/nothing/phone1/device.mk)
 
-# Inherit from the Nameless AOSP configuration.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from the Nameless Spark configuration.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -22,7 +22,7 @@ PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := phone1
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A063
-PRODUCT_NAME := aosp_phone1
+PRODUCT_NAME := spark_phone1
 PRODUCT_SYSTEM_DEVICE := Spacewar
 PRODUCT_SYSTEM_NAME := Spacewar
 
@@ -35,3 +35,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
     
 BUILD_FINGERPRINT := Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1669043157:user/release-keys
+
+# Spark config
+WITH_GAPPS := true
+WITH_FOD_ANIMATIONS := true
+TARGET_USES_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
